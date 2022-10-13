18dcc11af7a8b71b98c1394eb8ae30c60638265c
DiegoCadena <diego@graywolf.co>
2022-10-13 08:42:47 -0600

    ligar controlador con html

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
+  public photos = []
+  constructor() {
+    this.photos=[
+      {
+        name:"Foto 1",
+        description:"blandit cursus risus at ultrices mi tempus imperdiet nulla malesuada",
+        url:"https://picsum.photos/id/1016/300"
+      },
+      {
+        name:"Foto 2",
+        description:"blandit cursus risus ",
+        url:"https://picsum.photos/id/1026/300"
+      },
+      {
+        name:"Foto 3",
+        description:"mi tempus imperdiet nulla malesuada",
+        url:"https://picsum.photos/id/126/300"
+      }
+    ]
+  }
 
 }
