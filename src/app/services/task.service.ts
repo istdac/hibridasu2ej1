91a0653e92d6e0d54ecf0339d17f5b2d37ed627a
DiegoCadena <diego@graywolf.co>
2022-10-18 08:26:12 -0600

    insertar metodos servicio tareas

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -10,4 +10,14 @@ export class TaskService {
     this.tasks.push("Tarea 1");
     this.tasks.push("Tarea 2");
    }
+   //Añadir metodos para manejar tareas
+   public getTasks():string[]{
+    return this.tasks;
+   }
+   public addTask(task:string){
+    this.tasks.push(task);
+   }
+   public deleteTask(pos:number){
+    this.tasks.splice(pos,1);
+   }
 }
